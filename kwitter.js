b92913a4a287e5c65dc800fb090dbaa1967ad70b
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2021-06-19 16:53:21 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,12 @@
 function add_user(){
+    
 get_item=document.getElementById("username").value;
+
+if(get_item!=''){
     localStorage.setItem("username",get_item);
     window.location="kwitter_room.html";
+}
+else{
+document.getElementById("username").placeholder="Please type a username.";
+}
 }
